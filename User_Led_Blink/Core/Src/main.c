47c44f7d9db04b3811be886c5766d52c5c8263e7
hkaslan84_2 <hkaslan84_2@hkaslan84_2>
2025-10-25 01:18:40 +0300

    Small Changes in Main.c

--- User_Led_Blink/Core/Src/main.c
+++ User_Led_Blink/Core/Src/main.c
@@ -55,7 +55,7 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+// small changes
 /* USER CODE END 0 */
 
 /**

b0b635b63c8698ea0b43d112bb54255c60e88c2b
hkaslan84_2 <hkaslan84_2@hkaslan84_2>
2026-08-16 16:04:57 +0300

    trying commit

--- External_Interrupt_Led_Blink/Core/Src/main.c
+++ External_Interrupt_Led_Blink/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+//trying
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/

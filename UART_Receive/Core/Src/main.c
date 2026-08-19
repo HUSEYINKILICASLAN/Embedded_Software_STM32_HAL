5f264eb11738ba7398af858007c043ba3cde1dea
hkaslan84_2 <hkaslan84_2@hkaslan84_2>
2026-08-20 00:10:48 +0300

    update

--- UART_Receive/Core/Src/main.c
+++ UART_Receive/Core/Src/main.c
@@ -47,9 +47,9 @@ UART_HandleTypeDef huart1;
 /* USER CODE BEGIN PV */
 //uint8_t messageTX[] = "huseyin kilicaslan\r\n"; //bu degisken pooling modda kullanilacak
 //uint8_t messageRX[5];     //bu degisken pooling modda kullanilacak
-uint8_t rxChar;        //bu degisken interrupt modda kullanilacak
-char messageBuffer[BUFFER_SIZE];      //bu degisken interrupt modda kullanilacak
-uint8_t bufferIndex;    //bu degisken interrupt modda kullanilacak
+uint8_t rxChar; //bu degisken tek tek karakter almak icin, tek tek gelen karakterler messageBuffer dizisine arka arkaya eklenecek    //bu degisken interrupt modda kullanilacak
+char messageBuffer[BUFFER_SIZE]; //bu dizi, alinan karakterleri biriktirmek icin kullanilacak   //bu degisken interrupt modda kullanilacak
+uint8_t bufferIndex; //alinan karakterlerin dizideki yerini belirlemek icin kullanilacak  //bu degisken interrupt modda kullanilacak
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -95,7 +95,7 @@ int main(void) {
 	MX_GPIO_Init();
 	MX_USART1_UART_Init();
 	/* USER CODE BEGIN 2 */
-	HAL_UART_Receive_IT(&huart1, &rxChar, 1);
+	HAL_UART_Receive_IT(&huart1, &rxChar, 1); //haberlesmeyi baslatmak icin yazildi
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -107,9 +107,8 @@ int main(void) {
 		//if (HAL_UART_Receive(&huart1, messageRX, sizeof(messageRX), HAL_MAX_DELAY) == HAL_OK) {      //bu degisken pooling modda kullanilacak
 		//	HAL_UART_Transmit(&huart1, messageTX, sizeof(messageTX), HAL_MAX_DELAY);                   //bu degisken pooling modda kullanilacak
 		//}																							   //bu degisken pooling modda kullanilacak
-
-}
-/* USER CODE END 3 */
+	}
+	/* USER CODE END 3 */
 }
 
 /**
@@ -117,40 +116,40 @@ int main(void) {
  * @retval None
  */
 void SystemClock_Config(void) {
-RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
-/** Initializes the RCC Oscillators according to the specified parameters
- * in the RCC_OscInitTypeDef structure.
- */
-RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-	Error_Handler();
-}
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+	RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-/** Initializes the CPU, AHB and APB buses clocks
- */
-RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-		| RCC_CLOCKTYPE_PCLK1;
-RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
-	Error_Handler();
-}
-PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
-PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
-if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
-	Error_Handler();
-}
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+	PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
@@ -160,29 +159,29 @@ if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
  */
 static void MX_USART1_UART_Init(void) {
 
-/* USER CODE BEGIN USART1_Init 0 */
+	/* USER CODE BEGIN USART1_Init 0 */
 
-/* USER CODE END USART1_Init 0 */
+	/* USER CODE END USART1_Init 0 */
 
-/* USER CODE BEGIN USART1_Init 1 */
+	/* USER CODE BEGIN USART1_Init 1 */
 
-/* USER CODE END USART1_Init 1 */
-huart1.Instance = USART1;
-huart1.Init.BaudRate = 38400;
-huart1.Init.WordLength = UART_WORDLENGTH_8B;
-huart1.Init.StopBits = UART_STOPBITS_1;
-huart1.Init.Parity = UART_PARITY_NONE;
-huart1.Init.Mode = UART_MODE_TX_RX;
-huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-if (HAL_UART_Init(&huart1) != HAL_OK) {
-	Error_Handler();
-}
-/* USER CODE BEGIN USART1_Init 2 */
+	/* USER CODE END USART1_Init 1 */
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 38400;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART1_Init 2 */
 
-/* USER CODE END USART1_Init 2 */
+	/* USER CODE END USART1_Init 2 */
 
 }
 
@@ -192,48 +191,50 @@ if (HAL_UART_Init(&huart1) != HAL_OK) {
  * @retval None
  */
 static void MX_GPIO_Init(void) {
-GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-/* USER CODE BEGIN MX_GPIO_Init_1 */
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
 
-/* USER CODE END MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
 
-/* GPIO Ports Clock Enable */
-__HAL_RCC_GPIOA_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-/*Configure GPIO pin Output Level */
-HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, GPIO_PIN_RESET);
 
-/*Configure GPIO pin : User_Led_Pin */
-GPIO_InitStruct.Pin = User_Led_Pin;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
-HAL_GPIO_Init(User_Led_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : User_Led_Pin */
+	GPIO_InitStruct.Pin = User_Led_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	HAL_GPIO_Init(User_Led_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
 
-/* USER CODE END MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-if (huart->Instance == USART1) {
-	if (bufferIndex < BUFFER_SIZE - 1) {
-		messageBuffer[bufferIndex++] = rxChar;
-		messageBuffer[bufferIndex] = '0';
-	}
-	if (strstr(messageBuffer, LED_ON_COMMAND) != NULL) {
-		HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, 1);
-		bufferIndex = 0;
-		memset(messageBuffer, 0, BUFFER_SIZE);
-	} else if (strstr(messageBuffer, LED_OFF_COMMAND) != NULL) {
-		HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, 0);
-		bufferIndex = 0;
-		memset(messageBuffer, 0, BUFFER_SIZE);
-	}
-}
+	if (huart->Instance == USART1) { //hangi UART modulundan geldigini ayirtetmek icin yazildi
 
-HAL_UART_Receive_IT(&huart1, &rxChar, 1);
+		if (bufferIndex < BUFFER_SIZE - 1) { //buffer boyutunun bir eksigi kadar eklemek icin yazildi
+			messageBuffer[bufferIndex++] = rxChar; //gelen karakterleri arka arkaya diziye eklemek icin yazildi
+			messageBuffer[bufferIndex] = '\0';           //bitis karakteri
+		}
+
+		if (strstr(messageBuffer, LED_ON_COMMAND) != NULL) { //gelen karakterler dizisi, on komutuna esit mi kontrol etmek icin yazildi
+			HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, 1); //gelen karakter dizisi, on ise ledi yak
+			bufferIndex = 0;               //buffer index i sifirla
+			memset(messageBuffer, 0, BUFFER_SIZE); //meddageBuffer dizisini buffer size kadar sifirla
+		} else if (strstr(messageBuffer, LED_OFF_COMMAND) != NULL) { //gelen karakterler dizisi off komutuna esit mi kontrol etmek icin yazildi
+			HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, 0); //gelen karakter dizisi, off ise ledi sondur
+			bufferIndex = 0;                //buffer index i sifirla
+			memset(messageBuffer, 0, BUFFER_SIZE); //meddageBuffer dizisini buffer size kadar sifirla
+		}
+	}
+
+	HAL_UART_Receive_IT(&huart1, &rxChar, 1); //tekrar uart alimini acmak icin yazildi, eger tekrar acilmazsa tek karakteri alip koddan cikar
 }
 
 /* USER CODE END 4 */
@@ -243,12 +244,12 @@ HAL_UART_Receive_IT(&huart1, &rxChar, 1);
  * @retval None
  */
 void Error_Handler(void) {
-/* USER CODE BEGIN Error_Handler_Debug */
-/* User can add his own implementation to report the HAL error return state */
-__disable_irq();
-while (1) {
-}
-/* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**

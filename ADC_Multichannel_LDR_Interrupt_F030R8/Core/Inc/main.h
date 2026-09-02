b47f14703b1c4da7fbd35206850ebac6b3b568e2
hkaslan84_2 <hkaslan84_2@hkaslan84_2>
2026-09-02 21:47:05 +0300

    Initial commit

---
+++ ADC_Multichannel_LDR_Interrupt_F030R8/Core/Inc/main.h
@@ -0,0 +1,79 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Fourth_External_LED_Pin GPIO_PIN_1
+#define Fourth_External_LED_GPIO_Port GPIOC
+#define First_External_LED_Pin GPIO_PIN_1
+#define First_External_LED_GPIO_Port GPIOA
+#define Second_External_LED_Pin GPIO_PIN_4
+#define Second_External_LED_GPIO_Port GPIOA
+#define User_LED_Pin GPIO_PIN_5
+#define User_LED_GPIO_Port GPIOA
+#define Third_External_LED_Pin GPIO_PIN_0
+#define Third_External_LED_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
